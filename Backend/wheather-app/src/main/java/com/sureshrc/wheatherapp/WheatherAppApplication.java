334e5013bd81fb07505fe6f78961dbce2e1dafc7
Suresh Kukkala <sureshkukkala@primesoftinc.com>
2025-01-30 07:43:22 +0530

    Backend is initialized

---
+++ Backend/wheather-app/src/main/java/com/sureshrc/wheatherapp/WheatherAppApplication.java
@@ -0,0 +1,15 @@
+package com.sureshrc.wheatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheatherAppApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WheatherAppApplication.class, args);
+    }
+
+}
---
+++ Backend/wheather-app/src/test/java/com/sureshrc/wheatherapp/WheatherAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sureshrc.wheatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheatherAppApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
